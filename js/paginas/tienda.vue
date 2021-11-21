bbb27aac5c1fa0b7e6fe479aa90d02877b9abe54
Diogo15 <luisdiegore@hotmail.com>
2021-11-20 18:51:57 -0600

    haciendo feo form

--- js/paginas/tienda.vue
+++ js/paginas/tienda.vue
@@ -1,7 +1,13 @@
 <template>
   <transition name="fade">
     <div>
-        <h1>TIENDA</h1>
+        <h1>Formulario Ingresar</h1>
+
+        <input v-model="message" placeholder="editar" >
+        
+        <p v-if="mostrar">Message is: {{ message }}</p>
+
+        <p><input type="checkbox" v-model="mostrar"> Mostrar/Ocultar </p>
 
     </div>
   </transition>
@@ -9,6 +15,12 @@
 
 <script>
 module.exports = {
+  data: function() {
+    return{
+      mostrar: false,
+      message: "text por defecto"
+    }
+  },
   mounted () {
 
   }
