c4c75a785de7d6590c5a115d7b45c38a36203ec2
Diogo15 <luisdiegore@hotmail.com>
2021-11-20 19:20:20 -0600

    add the site base url from config-file
    (cherry picked from commit c9d51fb587b069c94eeb11b1b8a0816cb5bff3d6)

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <script>
+        <?php $config = json_decode(file_get_contents(dirname(__FILE__).'\php\config-local'), true); ?>
+        var site_url = "<?php echo $config["connection"]["xamp_path"]; ?>";
+    </script>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
