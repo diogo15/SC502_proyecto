aca9ab6bbb8ecec1927d9340b4a8c48bd961c2ff
Diogo15 <luisdiegore@hotmail.com>
2021-11-22 15:19:14 -0600

    inserts added

--- php/connectionDB.php
+++ php/connectionDB.php
@@ -63,5 +63,32 @@
             }
             return $resultArray;
         }
+
+        //  INSERT ON TABLE (Afecta todas las filas de la tabla)
+
+        public function insert($statement){
+            $results = $this -> connection -> query($statement);
+            
+            return $this -> connection -> affected_rows;
+        }
+
+        //  INSERT ON TABLE BY ID (Afectaría una sola fila) = UPDATE?
+
+        public function getById($statement){
+            $results = $this -> connection -> query($statement);
+            
+            $rows = $this -> connection -> affected_rows;
+
+            if ($rows >= 1) {
+
+                return $this -> connection -> insert_id;
+
+            } else {
+
+                return 0;
+
+            }
+        }
+        
     }
 ?>
