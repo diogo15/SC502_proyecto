41ac0694ab7820afaea8fe184b2e55c38fd7a221
Diogo15 <luisdiegore@hotmail.com>
2021-12-04 22:47:32 -0600

    Registro Fixes and updates

--- api/data-registro.php
+++ api/data-registro.php
@@ -1,11 +1,16 @@
 <?php
 require_once '../php/connectionDB.php';
 
-function registrar_usuario($_POST["nombre"],$_POST["password"],$_POST["correo"],$_POST["fechaNacimineto"],$_POST["passwordConfirm"],$_POST["telefono"]);
-{
+function registrar_usuario($nombre,$password,$correo,$fechaNacimineto,$passwordConfirm,$telefono){
     $conexion = new ConnectionDB();
-    $statement =$conexion->prepare ("INSERT INTO `usuarios` (`idUsuario`, `nombreUser`, `apellido1Usuario`, `apellido2Ususario`, `passwordUsuario`, `mailUsuario`, `direccion`, `idRol`, `idPais`, `idProvincia`) VALUES ('', '', '', '', '', '', '', '', '', '');");
-    $statement ->bind_param("issssssiii", &nombre, &apellido1, &apellido2, &password, &mail, &direccion, &rol, &pais, &provincia);
+    $rol = 1;
+    $pais = 1;
+    $provincia = 1;
+    $apellido1 = "";
+    $apellido2 = "";
+    $direccion = "San Jose, Costa Rica";
+    $statement =$conexion->prepare ("INSERT INTO `usuarios` (`nombreUser`, `apellido1Usuario`, `apellido2Ususario`, `passwordUsuario`, `mailUsuario`, `direccion`, `idRol`, `idPais`, `idProvincia`) VALUES ('?', '?', '?', '?', '?', '?', '?', '?', '?');");
+    $statement ->bind_param("ssssssiii", $nombre, $apellido1, $apellido2, $password, $correo, $direccion, $rol, $pais, $provincia);
     $product = $conexion->getAllData($statement);
 
 	return $product;
--- api/registro.php
+++ api/registro.php
@@ -1,29 +1,31 @@
 <?php
-
-header("Content-Type:application/json");
 require "data-registro.php";
+require_once "response.php";
+
+use IntoTheZone\JsonResponse;
+
 if(empty($_POST['nombre'])){
-	response(400,"Falta parametro",NULL);
+	new JsonResponse('badrequest', 'Falta parametro', NULL);
 	die();
 }
 if(empty($_POST['apellidos'])){
-	response(400,"Falta parametro",NULL);
+	new JsonResponse('badrequest', 'Falta parametro', NULL);
 	die();
 }
 if(empty($_POST['fechaNacimiento'])){
-	response(400,"Falta parametro",NULL);
+	new JsonResponse('badrequest', 'Falta parametro', NULL);
 	die();
 }
 if(empty($_POST['correo'])){
-	response(400,"Falta parametro",NULL);
+	new JsonResponse('badrequest', 'Falta parametro', NULL);
 	die();
 }
 if(empty($_POST['passwordConfirm'])){
-	response(400,"Falta parametro",NULL);
+	new JsonResponse('badrequest', 'Falta parametro', NULL);
 	die();
 }
 if(empty($_POST['telefono'])){
-	response(400,"Falta parametro",NULL);
+	new JsonResponse('badrequest', 'Falta parametro', NULL);
 	die();
 }
 
@@ -31,24 +33,11 @@ $usuario = registrar_usuario($_POST["nombre"],$_POST["password"],$_POST["correo"
 
 if(empty($usuario))
 {
-	response(200,"user not added",NULL);
+	new JsonResponse('ok', 'user not added', NULL);
 }
 else
 {
-	response(200,"user added",$usuario);
-}
-
-}
-function response($status,$status_message,$data)
-{
-	header("HTTP/1.1 ".$status);
-	
-	$response['status']=$status;
-	$response['status_message']=$status_message;
-	$response['data']=$data;
-	
-	$json_response = json_encode($response);
-	echo $json_response;
+	new JsonResponse('ok', 'user added', $usuario);
 }
 
 
