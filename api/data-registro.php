061f758e8f41b9033b6bc78bacee11932ad6e8b5
Inm0rt4LKoff33 <fabimv23@gmail.com>
2021-12-11 22:58:09 -0600

    Encrypt password register

--- api/data-registro.php
+++ api/data-registro.php
@@ -2,6 +2,7 @@
 require_once '../php/connectionDB.php';
 
 function registrar_usuario($nombre,$password,$correo,$fechaNacimineto,$passwordConfirm,$telefono){
+        
     $conexion = new ConnectionDB();
     $rol = 1;
     $pais = 1;
@@ -9,10 +10,12 @@ function registrar_usuario($nombre,$password,$correo,$fechaNacimineto,$passwordC
     $apellido1 = "";
     $apellido2 = "";
     $direccion = "San Jose, Costa Rica";
-    $query = "INSERT INTO usuarios (nombreUser, apellido1Usuario, apellido2Usuario, passwordUsuario, mailUsuario) VALUES (?,?,?,?,?);";
-    
-	$conexion->queryPrepared($query,[$nombre, $apellido1, $apellido2, $password, $correo]);
+
+    //$passEncrypt = $conexion->encript($password);
+
+    $query = "INSERT INTO usuarios (nombreUser, apellido1Usuario, apellido2Usuario, passwordUsuario, mailUsuario) VALUES (?, ?, ?, md5(?), ?);";
+
+    $conexion->queryPrepared($query,[$nombre, $apellido1, $apellido2, $password, $correo]);
 
     return false;
-	
 }
