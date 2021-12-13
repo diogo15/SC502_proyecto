0820d308858d49a5351acab270066d4161e817c1
Diogo15 <luisdiegore@hotmail.com>
2021-12-12 21:48:45 -0600

    Estilo banner agregado

--- js/componentes/banner.vue
+++ js/componentes/banner.vue
@@ -1,10 +1,10 @@
 <template>
 
 <div id="banner">
-  <img class="img-carousel3" v-bind:src="'img/productos/'+imagen"/>
-    <div class="banner1 h1">
-        <h1><slot></slot></h1>
-        <btn class="link"><router-link to="/tienda">Ver Mas</router-link></btn>
+  <img v-bind:src="'img/productos/'+imagen"/>
+    <div class="promoText">
+        <h2><slot></slot></h2>
+        <btn class="link white"><router-link to="/tienda">Ver Mas</router-link></btn>
     </div>
 </div>
 
@@ -23,34 +23,23 @@ module.exports = {
 
 #banner{
   position: relative;
-  
 }
-#banner .banner1{
+#banner .promoText{
   position: absolute;
   bottom: 5%;
-  left: 100px;
+  padding: 20px 50px 20px 5%;
   color: white;
+  background: #333;
+  border-right: 10px solid #C2814D;
 }
-#banner p{
-  font-size: 20px;
-  display: block;
-  color: rgb(255, 255, 255);
-  text-decoration: none;
-  padding: 10px;
-  margin-top: 20px;
-  border: 2px solid peru;
-  width: 25%;
-}
-#banner p:hover{
-  background: rgba(51, 51, 51, 0.6);
-  width: 25%;
-}
-#banner h1{
-  display: block;
-  line-height: 50%;
+#banner h2{
+  margin: 0 0 10px 0;
 }
 #banner img{
   width: 100%;
+}
+.promoText .button.link a{
+  color: #333;
 }
 
 
