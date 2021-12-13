708092076288a7a771f0f07a8a011866e2e8e7fc
Inm0rt4LKoff33 <fabimv23@gmail.com>
2021-12-13 14:47:46 -0600

    Correción en el store procedure :P

--- DB_scripts/getPedidoUsuario.sql
+++ DB_scripts/getPedidoUsuario.sql
@@ -1,12 +1,17 @@
 DELIMITER $$
 create procedure spGetUsuarioPedido(in usuario int)
 begin
-	SELECT pedidos.estadoPedido, pedidos.fecha,
-			detallepedidos.numLinea,  productos.nombreProducto, 
-            detallepedidos.cantidad, detallepedidos.totalLinea, pedidos.paga
+	SELECT pedidos.estadoPedido, 
+            pedidos.fecha,
+            detallepedidos.numLinea,
+            productos.nombreProducto,
+            detallepedidos.cantidad,
+            detallepedidos.totalLinea,
+            pedidos.paga
     from Pedidos, Productos, detallepedidos, usuarios 
 		where usuarios.idUsuario = usuario 
         AND productos.idProducto = detallepedidos.idProducto
+        AND pedidos.idPedido = detallepedidos.idPedido
         AND pedidos.idUsuario = usuarios.idUsuario;
 end;
 $$
